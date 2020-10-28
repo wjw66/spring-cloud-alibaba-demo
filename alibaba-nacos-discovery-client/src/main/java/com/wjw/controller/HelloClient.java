6f40a8602c5830a1a27628063982508887994ad7
01392915 <wangjianwei2@sfmail.sf-express.com>
2020-10-28 17:46:50 +0800

    nacos 服务消费者

--- alibaba-nacos-discovery-client/src/main/java/com/wjw/controller/HelloClient.java
+++ alibaba-nacos-discovery-client/src/main/java/com/wjw/controller/HelloClient.java
@@ -3,10 +3,8 @@ package com.wjw.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
